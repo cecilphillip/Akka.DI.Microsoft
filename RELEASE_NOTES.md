1d59876d365a9be658cc7804f618c71c2ae6f3f1
Michael Baird <mike@baird.rocks>
2018-03-13 12:55:42 -0500

    added build scripts

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+#### 1.3.5 March 13 2018 ####
+
+Support for Akka 1.3.5
---
+++ build.fsx
@@ -0,0 +1,138 @@
+#I @"tools/FAKE/tools"
+#r "FakeLib.dll"
+
+open System
+open System.IO
+open System.Text
+
+open Fake
+open Fake.DotNetCli
+open Fake.DocFxHelper
+
+// Variables
+let configuration = "Release"
+
+// Directories
+let output = __SOURCE_DIRECTORY__  @@ "bin"
+let outputTests = output @@ "TestResults"
+let outputBinaries = output @@ "binaries"
+let outputNuGet = output @@ "nuget"
+
+let buildNumber = environVarOrDefault "BUILD_NUMBER" "0"
+let preReleaseVersionSuffix = "beta" + (if (not (buildNumber = "0")) then (buildNumber) else "")
+let versionSuffix = 
+    match (getBuildParam "nugetprerelease") with
+    | "dev" -> preReleaseVersionSuffix
+    | _ -> ""
+
+Target "Clean" (fun _ ->
+    CleanDir output
+    CleanDir outputTests
+    CleanDir outputBinaries
+    CleanDir outputNuGet
+
+    CleanDirs !! "./**/bin"
+    CleanDirs !! "./**/obj"
+)
+
+Target "RestorePackages" (fun _ ->
+    DotNetCli.Restore
+        (fun p -> 
+            { p with
+                Project = "./Akka.DI.Microsoft.sln"
+                NoCache = false })
+)
+
+Target "Build" (fun _ ->
+        DotNetCli.Build
+            (fun p -> 
+                { p with
+                    Project = "./Akka.DI.Microsoft.sln"
+                    Configuration = configuration })
+)
+
+//--------------------------------------------------------------------------------
+// Tests targets 
+//--------------------------------------------------------------------------------
+
+Target "RunTests" (fun _ ->
+    ensureDirectory outputTests
+    let projects = !! "./**/*.Tests.csproj"
+
+    let runSingleProject project =
+        DotNetCli.RunCommand
+            (fun p -> 
+                { p with 
+                    WorkingDir = (Directory.GetParent project).FullName
+                    TimeOut = TimeSpan.FromMinutes 10. })
+                (sprintf "xunit -parallel none -teamcity -xml %s_xunit.xml" (outputTests @@ fileNameWithoutExt project)) 
+
+    projects |> Seq.iter (runSingleProject)
+)
+
+//--------------------------------------------------------------------------------
+// Nuget targets 
+//--------------------------------------------------------------------------------
+
+Target "CreateNuget" (fun _ ->
+    DotNetCli.Pack
+        (fun p -> 
+            { p with
+                Project = "./src/Akka.DI.Microsoft/Akka.DI.Microsoft.csproj"
+                Configuration = configuration
+                AdditionalArgs = ["--include-symbols"]
+                VersionSuffix = versionSuffix
+                OutputPath = outputNuGet })
+)
+
+Target "PublishNuget" (fun _ ->
+    let projects = !! "./bin/nuget/*.nupkg" -- "./bin/nuget/*.symbols.nupkg"
+    let apiKey = getBuildParamOrDefault "nugetkey" ""
+    let source = getBuildParamOrDefault "nugetpublishurl" ""
+    let symbolSource = getBuildParamOrDefault "symbolspublishurl" ""
+    let shouldPublishSymbolsPackages = not (symbolSource = "")
+
+    if (not (source = "") && not (apiKey = "") && shouldPublishSymbolsPackages) then
+        let runSingleProject project =
+            DotNetCli.RunCommand
+                (fun p -> 
+                    { p with 
+                        TimeOut = TimeSpan.FromMinutes 10. })
+                (sprintf "nuget push %s --api-key %s --source %s --symbol-source %s" project apiKey source symbolSource)
+
+        projects |> Seq.iter (runSingleProject)
+    else if (not (source = "") && not (apiKey = "") && not shouldPublishSymbolsPackages) then
+        let runSingleProject project =
+            DotNetCli.RunCommand
+                (fun p -> 
+                    { p with 
+                        TimeOut = TimeSpan.FromMinutes 10. })
+                (sprintf "nuget push %s --api-key %s --source %s" project apiKey source)
+
+        projects |> Seq.iter (runSingleProject)
+)
+
+//--------------------------------------------------------------------------------
+//  Target dependencies
+//--------------------------------------------------------------------------------
+
+Target "BuildRelease" DoNothing
+Target "All" DoNothing
+Target "Nuget" DoNothing
+
+// build dependencies
+"Clean" ==> "RestorePackages" ==> "Build" ==> "BuildRelease"
+
+// tests dependencies
+"Clean" ==> "RestorePackages" ==> "RunTests"
+
+// nuget dependencies
+"Clean" ==> "RestorePackages" ==> "Build" ==> "CreateNuget"
+"CreateNuget" ==> "PublishNuget" ==> "Nuget"
+
+// all
+"BuildRelease" ==> "All"
+"RunTests" ==> "All"
+"Nuget" ==> "All"
+
+RunTargetOrDefault "All"
